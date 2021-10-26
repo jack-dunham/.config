833476abb754ef3973ea496be392c51a7a8dee79
jack-dunham <ucapunh@ucl.ac.uk>
2021-10-26 13:39:03 +0100

    additions

--- lua/keybinds.lua
+++ lua/keybinds.lua
@@ -3,8 +3,38 @@ local wk = require('which-key')
 wk.register({
 	o = {
 		name = "open",
-		t = {"<cmd>CHADopen<cr>", "Open/close file tree"}
+		t = {"<cmd>NvimTreeToggle<cr>", "Open/close file tree"}
 	},
-	["-"] = {"<cmd>CHADopen --always-focus<cr>", "Focus file tree"}
+	["-"] = {"<cmd>NvimTreeFocus<cr>", "Focus file tree"}
 }, {prefix = "<leader>"})
 
+wk.register({
+	["g?"] = {"Toggle help"},
+	["<leader>-"] = {"<cmd>wincmd p<cr>", "Unfocus file tree"}
+}, {buffer = 2})
+
+
+wk.register({
+	p = {
+		name = "packer",
+		i = {"<cmd>PackerInstall<cr>", "Install"},
+		S = {"<cmd>PackerSource<cr>", "Source"},
+		s = {"<cmd>PackerSync<cr>", "Sync"},
+		u = {"<cmd>PackerUpdate<cr>", "Update"},
+		c = {"<cmd>PackerCompile<cr>", "Compile"},
+		p = {"<cmd>PackerProfile<cr>", "Profile"},
+		t = {"<cmd>PackerStatus<cr>", "Status"},
+		d = {"<cmd>PackerClean<cr>", "Clean"},
+	}
+}, {prefix = "<leader>"})
+
+wk.register({
+	g = {
+		name = "git",
+		g = {"<cmd>Git<cr>", "Status"},
+	}
+}, {prefix = "<leader>"})
+
+wk.register({
+	["g?"] = {"fugitive_g?", "Open keybind help"}
+},{})

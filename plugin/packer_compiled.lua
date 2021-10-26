49d4fa0659376e064f0884810ff062ec4545ebe0
jack-dunham <ucapunh@ucl.ac.uk>
2021-10-26 11:09:34 +0100

    update .gitignore

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/homes/jdunham/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/homes/jdunham/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/homes/jdunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/homes/jdunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/homes/jdunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/JackDunham/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/JackDunham/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/JackDunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/JackDunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/JackDunham/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,100 +71,96 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/LuaSnip"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/cmp-buffer"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp"
   },
   ["julia-vim"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/julia-vim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/julia-vim"
   },
   ["lspkind-nvim"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/lspkind-nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/lspkind-nvim"
   },
   ["lualine.nvim"] = {
     config = { "\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins/lualine\frequire\0" },
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/lualine.nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/lualine.nvim"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-cmp"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-cmp"
   },
   ["nvim-lsp-installer"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-lsp-installer"
   },
   ["nvim-lspconfig"] = {
     config = { "\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19nvim-lspconfig\frequire\0" },
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
   },
   ["nvim-tree.lua"] = {
     config = { "\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0" },
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
   },
   ["onedark.nvim"] = {
     config = { "\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins/onedark\frequire\0" },
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/onedark.nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/onedark.nvim"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/packer.nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/plenary.nvim"
   },
   ["vim-commentary"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/vim-commentary"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/vim-commentary"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/vim-fugitive"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/vim-fugitive"
   },
   ["vim-rhubarb"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/vim-rhubarb"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/vim-rhubarb"
   },
   vimtex = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/vimtex"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/vimtex"
   },
   ["which-key.nvim"] = {
     config = { "\27LJ\2\n(\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\rkeybinds\frequire\0" },
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/which-key.nvim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/which-key.nvim"
   },
   ["zepl.vim"] = {
     loaded = true,
-    path = "/homes/jdunham/.local/share/nvim/site/pack/packer/start/zepl.vim"
+    path = "/Users/JackDunham/.local/share/nvim/site/pack/packer/start/zepl.vim"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: lualine.nvim
-time([[Config for lualine.nvim]], true)
-try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins/lualine\frequire\0", "config", "lualine.nvim")
-time([[Config for lualine.nvim]], false)
 -- Config for: which-key.nvim
 time([[Config for which-key.nvim]], true)
 try_loadstring("\27LJ\2\n(\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\rkeybinds\frequire\0", "config", "which-key.nvim")
@@ -173,14 +169,18 @@ time([[Config for which-key.nvim]], false)
 time([[Config for onedark.nvim]], true)
 try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins/onedark\frequire\0", "config", "onedark.nvim")
 time([[Config for onedark.nvim]], false)
--- Config for: nvim-tree.lua
-time([[Config for nvim-tree.lua]], true)
-try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
-time([[Config for nvim-tree.lua]], false)
 -- Config for: nvim-lspconfig
 time([[Config for nvim-lspconfig]], true)
 try_loadstring("\27LJ\2\n.\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\19nvim-lspconfig\frequire\0", "config", "nvim-lspconfig")
 time([[Config for nvim-lspconfig]], false)
+-- Config for: nvim-tree.lua
+time([[Config for nvim-tree.lua]], true)
+try_loadstring("\27LJ\2\n;\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\14nvim-tree\frequire\0", "config", "nvim-tree.lua")
+time([[Config for nvim-tree.lua]], false)
+-- Config for: lualine.nvim
+time([[Config for lualine.nvim]], true)
+try_loadstring("\27LJ\2\n/\0\0\3\0\2\0\0046\0\0\0'\2\1\0B\0\2\1K\0\1\0\20plugins/lualine\frequire\0", "config", "lualine.nvim")
+time([[Config for lualine.nvim]], false)
 if should_profile then save_profiles() end
 
 end)

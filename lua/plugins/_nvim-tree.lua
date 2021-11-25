8d20ce2733d540d47314e76a9c21a07141f4beb6
jack-dunham <ucapunh@ucl.ac.uk>
2021-11-25 12:33:13 +0000

    format file

--- lua/plugins/_nvim-tree.lua
+++ lua/plugins/_nvim-tree.lua
@@ -1,33 +1,33 @@
 require("nvim-tree").setup({
-  update_cwd = true,
-  -- update_focused_file = {
-  --   enable = true,
-  --   update_cwd = true
-  -- },
-  view = {
-	  mappings = {
-		  list = {
-			  {key = "<leader>-", cb = ":wincmd p<cr>", mode = "n"}
-		  }
-	  }
-  }
+	update_cwd = true,
+	-- update_focused_file = {
+	--   enable = true,
+	--   update_cwd = true
+	-- },
+	view = {
+		mappings = {
+			list = {
+				{ key = "<leader>-", cb = ":wincmd p<cr>", mode = "n" },
+			},
+		},
+	},
 })
-local view  = require'nvim-tree.view'
+local view = require("nvim-tree.view")
 
-local _M={}
+local _M = {}
 _M.toggle_tree = function()
 	if view.win_open() then
-   		require'nvim-tree'.close()
-   		require'bufferline.state'.set_offset(0)
+		require("nvim-tree").close()
+		require("bufferline.state").set_offset(0)
 	else
-   		require'bufferline.state'.set_offset(31, 'File Explorer')
-   		require'nvim-tree'.find_file(true)
+		require("bufferline.state").set_offset(31, "File Explorer")
+		require("nvim-tree").find_file(true)
 	end
 end
 
 _M.focus_tree = function()
 	if view.win_open() then
-		require'nvim-tree'.focus()
+		require("nvim-tree").focus()
 	else
 		_M.toggle_tree()
 	end
